f5a167c01338b9aed85f4e0913ec5d1166a42517
gsquibbs <squibbsg@msu.edu>
2017-04-12 19:26:43 -0400

    updated function with event listener, added localstorage to remember theme.

--- java.js
+++ java.js
@@ -1,12 +1,4 @@
-function myfunc (color) {
-  var className = color.getAttribute('class')
-  if (className !== 'day') {
-    color.className = 'day'
-  } else {
-    color.className = 'night'
-  }
-}
-
+// counts page visits
 var pageVisits = document.getElementById('counter')
 
 if (window.localStorage.pagecount) {
@@ -15,3 +7,23 @@ if (window.localStorage.pagecount) {
   window.localStorage.pagecount = 1
 }
 pageVisits.textContent = document.write('This page has been visited by you ' + window.localStorage.pagecount + ' times.')
+
+var currentTheme = window.localStorage.getItem('currentTheme')
+if (currentTheme !== null) {
+  document.body.setAttribute('class', currentTheme)
+}
+
+// changes and saves theme
+var toggleButton = document.getElementById('toggle')
+
+toggleButton.addEventListener('click', function () {
+  if (document.body.getAttribute('class') !== 'day') {
+    currentTheme = 'day'
+    document.body.setAttribute('class', currentTheme)
+    window.localStorage.setItem('currentTheme', currentTheme)
+  } else {
+    currentTheme = 'night'
+    document.body.setAttribute('class', currentTheme)
+    window.localStorage.setItem('currentTheme', currentTheme)
+  }
+})
